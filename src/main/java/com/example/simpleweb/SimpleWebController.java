e8bec3c0fd9bd57151ed7e34b2c58c21339f31e8
Thuy Luu <thuy.luu@sjsu.edu>
2022-12-07 13:55:27 -0800

    Implemented User routes

--- src/main/java/com/example/simpleweb/SimpleWebController.java
+++ src/main/java/com/example/simpleweb/SimpleWebController.java
@@ -1,13 +1,46 @@
 package com.example.simpleweb;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.hibernate.ObjectNotFoundException;
+//import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
 
 @RestController
 public class SimpleWebController {
 
-    @GetMapping("/")
-    public User index(){
-        return new User("Thuy","thuyluu9595@gmail.com", "abc123");
+    private UserRepository userRepository;
+
+    public SimpleWebController(UserRepository userRepository) {
+        this.userRepository = userRepository;
     }
+
+    @GetMapping("/users")
+    public List<User> FindAllUsers(){
+        return userRepository.findAll();
+    }
+
+    @GetMapping("/users/{id}")
+    public Optional<User> FindUser(@PathVariable int id){
+        Optional<User> user = userRepository.findById(id);
+        if(user.isEmpty()){
+            throw new ObjectNotFoundException(user,"Oject not found");
+        }
+        return user;
+    }
+
+    @PostMapping("/users")
+    public Message CreateUser(@RequestBody User user){
+        userRepository.save(user);
+        return new Message("User created successfully");
+    }
+
+    @DeleteMapping("/users/{id}")
+    public Message DeleteUser(@PathVariable int id){
+        userRepository.deleteById(id);
+        return new Message("User deleted successfully");
+    }
+
+
 }
--- src/main/java/com/example/simpleweb/User.java
+++ src/main/java/com/example/simpleweb/User.java
@@ -1,16 +1,14 @@
 package com.example.simpleweb;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 @Entity
+@Table(name = "userinfo")
 public class User {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id=1;
     private String name;
     private String email;
     private String password;
